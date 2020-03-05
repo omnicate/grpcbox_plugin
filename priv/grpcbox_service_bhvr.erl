2cd278aeb9fd2637f1840f24a913039ba08bf0a8
Konstantin Tcepliaev <f355@f355.org>
2020-03-05 15:49:57 +0000

    support asynchronous responses to unary requests.

--- priv/grpcbox_service_bhvr.erl
+++ priv/grpcbox_service_bhvr.erl
@@ -8,8 +8,7 @@
 -module({{module_name}}_bhvr).
 
 {{#methods}}
-%% {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
+%% @doc {{^input_stream}}{{^output_stream}}Unary RPC{{/output_stream}}{{/input_stream}}
 -callback {{method}}({{^input_stream}}{{#output_stream}}{{pb_module}}:{{input}}(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{#input_stream}}{{^output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{#output_stream}}reference(), grpcbox_stream:t(){{/output_stream}}{{/input_stream}}{{^input_stream}}{{^output_stream}}ctx:t(), {{pb_module}}:{{input}}(){{/output_stream}}{{/input_stream}}) ->
-    {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:{{output}}(), ctx:t()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
-
+    {{#output_stream}}ok{{/output_stream}}{{^output_stream}}{ok, {{pb_module}}:{{output}}(), ctx:t()} | {noreply, ctx:t()}{{/output_stream}} | grpcbox_stream:grpc_error_response().
 {{/methods}}
